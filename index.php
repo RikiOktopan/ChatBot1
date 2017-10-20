b4536fc49aeb9838daeb2fd88bb9702c38fcc571
RikiOktopan <rikioktopan1@gmail.com>
2017-10-20 17:20:00 +0700

    Create index.php

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 /*
 copyright @ medantechno.com
 Modified by Ilyasa
-And Modified by Farzain - zFz ( Faraaz )
+And Modified by RikiOktopan
 2017
 */
 require_once('./line_class.php');
 
-$channelAccessToken = 'ACCESS TOKEN ENTE'; //Your Channel Access Token
-$channelSecret = 'SECRET TOKEN ENTE';//Your Channel Secret
+$channelAccessToken = 'lCLx6A1xEzqDLT5/MlizmbyApl9tEdddkl6unVh4dOl/whDihYdxSkB6n1Vy9rlcY73aFTcXJeGvilglnx/yHX5GjDlu3zwZMjMfwfe0PzhlxkpAYFTMHYEity3OOvJ/a03VWCSKMpvOZakJv94KvAdB04t89/1O/w1cDnyilFU='; //Your Channel Access Token
+$channelSecret = 'Sm3f92f0d4ff548702d574f57ef480bb56';//Your Channel Secret
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
@@ -26,7 +26,7 @@ if($message['type']=='sticker')
 							'messages' => array(
 								array(
 										'type' => 'text',									
-										'text' => 'Terima Kasih Stikernya.'										
+										'text' => 'Sticker mulu ga bisa ngetik ya'										
 									
 									)
 							)
@@ -35,7 +35,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'API KEY SIMSIMI ENTE'; //API SimSimi
+$key = 'c2ab9b79-0690-467f-a2ff-bb2d9464139c'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
